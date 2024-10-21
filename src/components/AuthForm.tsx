97b05e3235c6844d60243f1b82f4f99393af00aa
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-10-21 22:44:36 +0300

    fix dependencies

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Icon } from "@iconify/react";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
--- src/components/ChangeUserDataForm.tsx
+++ src/components/ChangeUserDataForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { User } from "../types/user";
 import { getUserInfo, updateUserInfo } from "../api/users";
